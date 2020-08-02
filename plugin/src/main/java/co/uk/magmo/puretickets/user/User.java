33825a31dccc689827077e06284ed87bde52ef62
broccolai <josh@magmo.co.uk>
2020-08-02 18:36:22 +0100

    Match user constructors to base user

--- plugin/src/main/java/co/uk/magmo/puretickets/user/User.java
+++ plugin/src/main/java/co/uk/magmo/puretickets/user/User.java
@@ -1,11 +1,20 @@
 package co.uk.magmo.puretickets.user;
 
-import co.uk.magmo.corn.spigot.BaseUser;
+import ai.broccol.corn.spigot.BaseUser;
+import ai.broccol.corn.spigot.locale.LocaleManager;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.UUID;
 
 public class User extends BaseUser {
-    public User(@NotNull CommandSender sender) {
-        super(sender);
+
+    public User(@NotNull final LocaleManager localeManager, @NotNull final CommandSender sender) {
+        super(localeManager, sender);
+    }
+
+    public User(@NotNull final LocaleManager localeManager, @Nullable final UUID uuid) {
+        super(localeManager, uuid);
     }
 }
